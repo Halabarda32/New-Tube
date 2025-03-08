cea81c0e20985c4fff6a8dbc02b645e50fe50f27
Krzysiek <krzysztof.kierylowski@gmail.com>
2025-03-08 21:28:11 +0100

    created procedures & trpc routers  for users videos

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
 import { pgTable, text, timestamp, uniqueIndex, uuid } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
 
 export const users = pgTable(
 	'users',
@@ -13,6 +14,10 @@ export const users = pgTable(
 	t => [uniqueIndex('clerk_id_idx').on(t.clerkId)]
 )
 
+export const userRelations = relations(users, ({ many }) => ({
+	videos: many(videos),
+}))
+
 export const categories = pgTable(
 	'categories',
 	{
@@ -24,3 +29,34 @@ export const categories = pgTable(
 	},
 	t => [uniqueIndex('name_idx').on(t.name)]
 )
+
+export const categoryRelations = relations(users, ({ many }) => ({
+	videos: many(videos),
+}))
+
+export const videos = pgTable('videos', {
+	id: uuid('id').primaryKey().defaultRandom(),
+	title: text('title').notNull(),
+	description: text('description'),
+	userId: uuid('user_id')
+		.references(() => users.id, {
+			onDelete: 'cascade',
+		})
+		.notNull(),
+	categoryid: uuid('category_Id').references(() => categories.id, {
+		onDelete: 'set null'
+	}),
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+	updatedAt: timestamp('updated_at').defaultNow().notNull(),
+})
+
+export const videoRelations = relations(videos, ({ one }) => ({
+	user: one(users, {
+		fields: [videos.userId],
+		references: [users.id],
+	}),
+	category: one(categories, {
+		fields: [videos.categoryid],
+		references: [categories.id],
+	}),
+}))
