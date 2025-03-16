9c96169d5c37a165fb3591b2699e9509aa6fde4a
Krzysiek <krzysztof.kierylowski@gmail.com>
2025-03-16 17:22:51 +0100

    connected video form to drizzle backend

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -63,6 +63,10 @@ export const videos = pgTable('videos', {
 	updatedAt: timestamp('updated_at').defaultNow().notNull(),
 })
 
+export const videoInsertSchema = createInsertSchema(videos)
+export const videoUpdateSchema = createUpdateSchema(videos)
+export const videoSelectSchema = createSelectSchema(videos)
+
 export const videoRelations = relations(videos, ({ one }) => ({
 	user: one(users, {
 		fields: [videos.userId],
--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -7,11 +7,14 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { z } from 'zod'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { ErrorBoundary } from 'react-error-boundary'
 import { MoreVerticalIcon, TrashIcon } from 'lucide-react'
+import { videoUpdateSchema } from '@/db/schema'
+import { toast } from 'sonner'
 
 interface FormSectionProps {
 	videoId: string
@@ -36,36 +39,119 @@ const FormSectionSkeleton = () => {
 }
 
 const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
+	const utils = trpc.useUtils()
 	const [video] = trpc.studio.getOne.useSuspenseQuery({ id: videoId })
+	const [categories] = trpc.categories.getMany.useSuspenseQuery()
 
-    const form = useForm({
-        defaultValues: video
-    })
+	const update = trpc.videos.update.useMutation({
+		onSuccess: () => {
+			utils.studio.getMany.invalidate()
+			utils.studio.getOne.invalidate({ id: videoId })
+			toast.success('Video updated')
+		},
+		onError: () => {
+			toast.error('Something went wrong')
+		},
+	})
+
+	const form = useForm<z.infer<typeof videoUpdateSchema>>({
+		resolver: zodResolver(videoUpdateSchema),
+		defaultValues: video,
+	})
+
+	const onSubmit = (data: z.infer<typeof videoUpdateSchema>) => {
+		update.mutate(data)
+	}
 
 	return (
-		<div className="flex items-center justify-between mb-6">
-			<div>
-				<h1 className="text-2lx font-bold">Video details</h1>
-				<p className="text-xs text-muted-foreground">Menage your video details</p>
-			</div>
-			<div className="flex items-center gap-x-2">
-				<Button type="submit" disabled={false}>
-					Save
-				</Button>
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button variant={'ghost'} size={'icon'}>
-							<MoreVerticalIcon />
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)}>
+				<div className="flex items-center justify-between mb-6">
+					<div>
+						<h1 className="text-2lx font-bold">Video details</h1>
+						<p className="text-xs text-muted-foreground">Menage your video details</p>
+					</div>
+					<div className="flex items-center gap-x-2">
+						<Button type="submit" disabled={update.isPending}>
+							Save
 						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end">
-						<DropdownMenuItem className="cursor-pointer">
-							<TrashIcon className="size-4 mr-2" />
-							Delete
-						</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			</div>
-		</div>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Button variant={'ghost'} size={'icon'}>
+									<MoreVerticalIcon />
+								</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent align="end">
+								<DropdownMenuItem className="cursor-pointer">
+									<TrashIcon className="size-4 mr-2" />
+									Delete
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</div>
+				</div>
+				<div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
+					<div className="space-y-8 lg:col-span-3">
+						<FormField
+							control={form.control}
+							name={'title'}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Title</FormLabel>
+									<FormControl>
+										<Input {...field} placeholder="Add a title of your video" />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name={'description'}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Description</FormLabel>
+									<FormControl>
+										<Textarea
+											{...field}
+											value={field.value ?? ''}
+											placeholder="Add a title of your video"
+											rows={10}
+											className="resize-none pr-10"
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						{/* TODO ADD THUMBNAIL FIELD HERE */}
+						<FormField
+							control={form.control}
+							name={'categoryid'}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Category</FormLabel>
+									<Select onValueChange={field.onChange} defaultValue={field.value ?? undefined}>
+										<FormControl>
+											<SelectTrigger>
+												<SelectValue placeholder={'Select a category'} />
+											</SelectTrigger>
+										</FormControl>
+										<SelectContent>
+											{categories.map(category => (
+												<SelectItem key={category.id} value={category.id}>
+													{category.name}
+												</SelectItem>
+											))}
+										</SelectContent>
+									</Select>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+				</div>
+			</form>
+		</Form>
 	)
 }
