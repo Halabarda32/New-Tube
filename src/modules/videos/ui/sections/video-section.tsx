652ca34574fffa2024c52ffe1759f1cb2241b408
Krzysiek <krzysztof.kierylowski@gmail.com>
2025-03-30 13:14:51 +0200

    created videoViews schema, procedure & triggered video view creation on video play

--- src/modules/videos/ui/sections/video-section.tsx
+++ src/modules/videos/ui/sections/video-section.tsx
@@ -7,6 +7,7 @@ import { ErrorBoundary } from 'react-error-boundary'
 import { VideoPlayer } from '../components/video-player'
 import { VideoBanner } from '../components/video-banner'
 import { VideoTopRow } from '../components/video-top-row'
+import { useAuth } from '@clerk/nextjs'
 
 interface VideoSectionProps {
 	videoId: string
@@ -23,7 +24,21 @@ export const VideoSection = ({ videoId }: VideoSectionProps) => {
 }
 
 const VideoSectionSuspense = ({ videoId }: VideoSectionProps) => {
+	const { isSignedIn } = useAuth()
+	const utils = trpc.useUtils()
 	const [video] = trpc.videos.getOne.useSuspenseQuery({ id: videoId })
+
+	const createView = trpc.videoViews.create.useMutation({
+		onSuccess: () => {
+			utils.videos.getOne.invalidate({ id: videoId })
+		},
+	})
+
+	const handlePlay = () => {
+		if (!isSignedIn) return
+
+		createView.mutate({ videoId })
+	}
 
 	return (
 		<>
@@ -32,10 +47,10 @@ const VideoSectionSuspense = ({ videoId }: VideoSectionProps) => {
 					'aspect-video bg-black rounded-xl overflow-hidden relative',
 					video.muxStatus !== 'ready' && 'rounded-b-none'
 				)}>
-				<VideoPlayer autoPlay onPlay={() => {}} playbackId={video.muxPlaybackId} thumbnailUrl={video.thumbnailUrl} />
+				<VideoPlayer autoPlay onPlay={handlePlay} playbackId={video.muxPlaybackId} thumbnailUrl={video.thumbnailUrl} />
 			</div>
-            <VideoBanner status={video.muxStatus}/>
-            <VideoTopRow video={video}/>
+			<VideoBanner status={video.muxStatus} />
+			<VideoTopRow video={video} />
 		</>
 	)
 }
